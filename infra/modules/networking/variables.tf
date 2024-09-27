2606ee548ef231abb7654eadefeb0dbbb3d8215e
ramjeerai <110187607+rairamjee@users.noreply.github.com>
2024-09-27 19:52:48 +0530

    feat: added networking module

---
+++ infra/modules/networking/variables.tf
@@ -0,0 +1,26 @@
+variable "vpc_config" {
+  type = object({
+    name       = string
+    cidr_block = string
+  })
+
+  validation {
+    condition     = can(cidrnetmask(var.vpc_config.cidr_block))
+    error_message = "The vpc_config option must contain a valid CIDR block"
+  }
+}
+
+variable "subnet_config" {
+  type = map(object({
+    cidr_block = string
+    public     = optional(bool, false)
+    az         = string
+  }))
+
+  validation {
+    condition = alltrue([
+      for config in var.subnet_config : can(cidrnetmask(config.cidr_block))
+    ])
+    error_message = "The subnet config option must contain a valid CIDR block"
+  }
+}
