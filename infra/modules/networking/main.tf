93e9ebe1d57cfaead32604df608fe683aa73a33a
vikashjman <vikaskumar@jmangroup.com>
2024-09-28 17:04:27 +0530

    updated networking module to restore previous version

--- infra/modules/networking/main.tf
+++ infra/modules/networking/main.tf
@@ -8,69 +8,51 @@ locals {
   }
 }
 
-data "aws_availability_zones" "available" {
+data "aws_availability_zones" "avaliable" {
   state = "available"
 }
-
 resource "aws_vpc" "this" {
-  count      = var.create_networking ? 1 : 0
   cidr_block = var.vpc_config.cidr_block
-  tags = merge(var.tags, {
+
+  tags = {
     Name = var.vpc_config.name
-    Type = "Networking"
-  })
+  }
 }
 
 resource "aws_subnet" "this" {
-  for_each = var.create_networking ? var.subnet_config : {}
-  vpc_id   = aws_vpc.this[0].id
+  for_each          = var.subnet_config
+  vpc_id            = aws_vpc.this.id
   availability_zone = each.value.az
-  cidr_block        = each.value.cidr_block
-  tags = merge(var.tags, {
+
+  cidr_block = each.value.cidr_block
+  tags = {
     Name = each.key
-    Type = "Networking"
-  })
+  }
 
   lifecycle {
     precondition {
-      condition     = contains(data.aws_availability_zones.available.names, each.value.az)
+      condition     = contains(data.aws_availability_zones.avaliable.names, each.value.az)
       error_message = "Invalid AZ"
     }
   }
-
-  depends_on = [aws_vpc.this]
 }
+
 
 resource "aws_internet_gateway" "this" {
-  for_each = var.create_networking && length(local.public_subnets) > 0 ? { "default" = 1 } : {}
-  vpc_id   = aws_vpc.this[0].id
-  tags     = merge(var.tags, {
-    Type = "Networking"
-  })
-
-  depends_on = [aws_vpc.this]
+  count  = length(local.public_subnets) > 0 ? 1 : 0
+  vpc_id = aws_vpc.this.id
 }
-
 resource "aws_route_table" "public_rtb" {
-  for_each = var.create_networking && length(local.public_subnets) > 0 ? { "default" = 1 } : {}
-  vpc_id   = aws_vpc.this[0].id
-
+  count  = length(local.public_subnets) > 0 ? 1 : 0
+  vpc_id = aws_vpc.this.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.this["default"].id
+    gateway_id = aws_internet_gateway.this[0].id
   }
-
-  tags = merge(var.tags, {
-    Type = "Networking"
-  })
-
-  depends_on = [aws_vpc.this, aws_internet_gateway.this]
 }
 
 resource "aws_route_table_association" "public_rtb" {
-  for_each       = var.create_networking ? local.public_subnets : {}
+  for_each       = local.public_subnets
   subnet_id      = aws_subnet.this[each.key].id
-  route_table_id = aws_route_table.public_rtb["default"].id
-
-  depends_on = [aws_vpc.this, aws_route_table.public_rtb]
+  route_table_id = aws_route_table.public_rtb[0].id
 }
