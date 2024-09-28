f83c8322dcb2a641975eb0a44e7573386303fa2b
vikashjman <vikaskumar@jmangroup.com>
2024-09-28 17:04:27 +0530

    updated networking module to restore previous version

--- infra/modules/networking/outputs.tf
+++ infra/modules/networking/outputs.tf
@@ -3,33 +3,32 @@
 # 3. Private subnets - subnet_key => { subnet_id, availability_zone }
 
 locals {
-  output_public_subnets = var.create_networking ? {
+  output_public_subnets = {
     for key in keys(local.public_subnets) : key => {
       subnet_id         = aws_subnet.this[key].id
       availability_zone = aws_subnet.this[key].availability_zone
     }
-  } : {}
+  }
 
-  output_private_subnets = var.create_networking ? {
+  output_private_subnets = {
     for key in keys(local.private_subnets) : key => {
       subnet_id         = aws_subnet.this[key].id
       availability_zone = aws_subnet.this[key].availability_zone
     }
-  } : {}
+  }
 }
-
 
 output "vpc_id" {
   description = "The AWS ID from the created VPC"
-  value       = var.create_networking ? aws_vpc.this[0].id : null
+  value       = aws_vpc.this.id
 }
 
 output "public_subnets" {
   description = "The ID and the availability zone of public subnets."
-  value       = var.create_networking ? local.output_public_subnets : {}
+  value       = local.output_public_subnets
 }
 
 output "private_subnets" {
   description = "The ID and the availability zone of private subnets."
-  value       = var.create_networking ? local.output_private_subnets : {}
+  value       = local.output_private_subnets
 }
