92bf89068ec17d51de32278d46f259062fb4a18c
vikashjman <vikaskumar@jmangroup.com>
2024-09-28 16:18:43 +0530

    fix: updated networking module to have  variable create_networking

--- infra/modules/networking/main.tf
+++ infra/modules/networking/main.tf
@@ -15,21 +15,21 @@ data "aws_availability_zones" "available" {
 resource "aws_vpc" "this" {
   count      = var.create_networking ? 1 : 0
   cidr_block = var.vpc_config.cidr_block
-  tags = {
+  tags = merge(var.tags, {
     Name = var.vpc_config.name
     Type = "Networking"
-  }
+  })
 }
 
 resource "aws_subnet" "this" {
-  for_each          = var.subnet_config
-  vpc_id            = aws_vpc.this[0].id
+  for_each = var.create_networking ? var.subnet_config : {}
+  vpc_id   = aws_vpc.this[0].id
   availability_zone = each.value.az
   cidr_block        = each.value.cidr_block
-  tags = {
+  tags = merge(var.tags, {
     Name = each.key
     Type = "Networking"
-  }
+  })
 
   lifecycle {
     precondition {
@@ -42,30 +42,35 @@ resource "aws_subnet" "this" {
 }
 
 resource "aws_internet_gateway" "this" {
-  count  = length(local.public_subnets) > 0 ? 1 : 0
-  vpc_id = aws_vpc.this[0].id
-  tags = {
+  for_each = var.create_networking && length(local.public_subnets) > 0 ? { "default" = 1 } : {}
+  vpc_id   = aws_vpc.this[0].id
+  tags     = merge(var.tags, {
     Type = "Networking"
-  }
+  })
+
   depends_on = [aws_vpc.this]
 }
 
 resource "aws_route_table" "public_rtb" {
-  count  = length(local.public_subnets) > 0 ? 1 : 0
-  vpc_id = aws_vpc.this[0].id
+  for_each = var.create_networking && length(local.public_subnets) > 0 ? { "default" = 1 } : {}
+  vpc_id   = aws_vpc.this[0].id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.this[0].id
+    gateway_id = aws_internet_gateway.this["default"].id
   }
+
+  tags = merge(var.tags, {
+    Type = "Networking"
+  })
 
   depends_on = [aws_vpc.this, aws_internet_gateway.this]
 }
 
 resource "aws_route_table_association" "public_rtb" {
-  for_each       = local.public_subnets
+  for_each       = var.create_networking ? local.public_subnets : {}
   subnet_id      = aws_subnet.this[each.key].id
-  route_table_id = aws_route_table.public_rtb[0].id
+  route_table_id = aws_route_table.public_rtb["default"].id
 
   depends_on = [aws_vpc.this, aws_route_table.public_rtb]
 }
