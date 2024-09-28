b6d6d5111a51bab896a56c63fc30acdaac846d14
vikashjman <vikaskumar@jmangroup.com>
2024-09-28 13:17:25 +0530

    chore: using PROVISIONED by default in biliing mode, feels like i have already done that

--- infra/modules/database/dyanmodb/main.tf
+++ infra/modules/database/dyanmodb/main.tf
@@ -1,6 +1,6 @@
 resource "aws_dynamodb_table" "dynamodb_table" {
   name         = var.table_name
-  billing_mode = "PROVISIONED"
+  billing_mode = var.billing_mode
   hash_key     = var.hash_key
 
   attribute {
