11b5d421769294af9669ac3c75c2fc302d199e4e
vikashjman <vikaskumar@jmangroup.com>
2024-09-29 12:06:08 +0530

    feat: actions file for starting up infrastructure with context as input

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,21 +1,23 @@
-# output "public_subnets" {
-#   description = "A list of public subnets in the VPC"
-#   value       = module.terraplay-vpc[0].public_subnets
-# }
+# Output for public subnets, only if we are in the global workspace
+output "public_subnets" {
+  description = "A list of public subnets in the VPC"
+  value       = terraform.workspace == "terraplay@global" ? module.terraplay-vpc[0].public_subnets : null
+}
 
-# output "security_group_id" {
-#   description = "The ID of the security group for the game server"
-#   value       = module.game-server-firewall[0].security_group_id
-# }
+# Output for security group ID, only if we are in the global workspace
+output "security_group_id" {
+  description = "The ID of the security group for the game server"
+  value       = terraform.workspace == "terraplay@global" ? module.game-server-firewall[0].security_group_id : null
+}
 
-# output "sg_vpc_id" {
-#   value = module.game-server-firewall[0].sg_vpc
-# }
+# Output for VPC ID, only if we are in the global workspace
+output "vpc" {
+  description = "The VPC ID"
+  value       = terraform.workspace == "terraplay@global" ? module.terraplay-vpc[0].vpc_id : null
+}
 
-# output "vpc" {
-#   value = module.terraplay-vpc[0].vpc_id
-# }
-
-# output "aws_key_name" {
-#   value = module.ssh_key[0].aws_key_name
-# }
+# Output for AWS key name, only if we are in the global workspace
+output "aws_key_name" {
+  description = "The AWS key name used for SSH"
+  value       = terraform.workspace == "terraplay@global" ? module.ssh_key[0].aws_key_name : null
+}
