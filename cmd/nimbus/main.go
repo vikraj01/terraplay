d9918db5882b4be7f1ae9040e9a6cca09b5678c8
vikashjman <vikaskumar@jmangroup.com>
2024-09-30 02:11:54 +0530

    feat: change in start game yaml

--- cmd/nimbus/main.go
+++ cmd/nimbus/main.go
@@ -1,9 +1,35 @@
 package main
 
 import (
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/vikraj01/terraplay/internals/bot"
+	"github.com/vikraj01/terraplay/internals/webhook"
 )
 
 func main() {
-	bot.StartBot()
+
+	go func(){
+		bot.StartBot()
+	}()
+
+	go func(){
+		http.HandleFunc("/webhook", webhook.HandleWebhook)
+		log.Println("Webhook server listening on port: 8080")
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatalf("Webhook server failed: %v", err)
+		}
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+	<-stop
+
+	log.Println("Shutting down both Discord bot and webhook listener...")
+
 }
