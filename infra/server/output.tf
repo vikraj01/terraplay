f7b4fe72141e462f57ba38551505a70a5118cdc4
vikashjman <vikaskumar@jmangroup.com>
2024-09-29 23:30:05 +0530

    feat: updated index of the game_server

--- infra/server/output.tf
+++ infra/server/output.tf
@@ -5,10 +5,10 @@ output "instance_id" {
 
 output "public_ip" {
   description = "The public IP of the EC2 instance"
-  value       = module.game_server[1].public_ip
+  value       = module.game_server[0].public_ip
 }
 
 output "private_ip" {
   description = "The private IP of the EC2 instance"
-  value       = module.game_server[2].private_ip
+  value       = module.game_server[0].private_ip
 }
