fa505401a82d4b177d0ba70b435dfbc1cd8df807
vikashjman <vikaskumar@jmangroup.com>
2024-09-30 15:18:50 +0530

    feat: update

--- infra/scripts/start.sh
+++ infra/scripts/start.sh
@@ -11,4 +11,6 @@ WORKSPACE_NAME="$USER_ID@$GAME"
 terraform init --backend-config="./env/backend.conf" --backend-config="key=terraform.tfstate"
 terraform workspace select "$WORKSPACE_NAME" || terraform workspace new "$WORKSPACE_NAME"
 terraform apply -var-file="env/${GAME}.tfvars" -var-file="env/common/terraform.tfvars" -auto-approve
-terraform output -json
+terraform output -json | jq 'with_entries(.value |= .value)' > output.json
+cat output.json
+rm output.json
