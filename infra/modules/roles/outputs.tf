ac169067097a5a73988d6755973845b6dd6e5a0c
vikashjman <vikaskumar@jmangroup.com>
2024-10-03 03:10:11 +0530

    feat: roles module create

---
+++ infra/modules/roles/outputs.tf
@@ -0,0 +1,9 @@
+output "role_arn" {
+  description = "The ARN of the IAM role"
+  value       = aws_iam_role.role.arn
+}
+
+output "role_name" {
+  description = "The name of the IAM role"
+  value       = aws_iam_role.role.name
+}
