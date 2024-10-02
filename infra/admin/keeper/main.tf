0d880bdbdff8e38b77828d7f4f67eba06fe27e68
vikashjman <vikaskumar@jmangroup.com>
2024-10-03 02:46:42 +0530

    feat: update bot server

--- infra/admin/keeper/main.tf
+++ infra/admin/keeper/main.tf
@@ -23,36 +23,3 @@ module "terraform_state_lock" {
     Name = var.table_name
   })
 }
-
-module "bot_server" {
-  source        = "../../modules/compute"
-  ami           = data.aws_ami.amazon_linux.id
-  instance_name = nimbus_bot_server
-  instance_type = var.instance_type
-  subnet_id     = ""
-  security_groups = [  ]
-  key_name = ""
-  
-}
-# ource          = "../modules/compute"
-#   count           = var.game != "" ? 1 : 0
-#   ami             = data.aws_ami.amazon_linux.id
-#   instance_name   = "${var.game}-server-${terraform.workspace}"
-#   instance_type   = var.instance_type
-#   subnet_id       = var.subnet_id
-#   security_groups = var.security_group_ids
-#   assign_eip      = var.assign_eip
-#   key_name        = var.key_name
-
-#   ebs_volumes = var.ebs_volumes
-
-#   user_data = file("server/config/${lower(var.game)}.sh")
-
-#   ec2_tags = merge(
-#     {
-#       Name = "${var.game}-server-${terraform.workspace}",
-#       Game = var.game,
-#       Type = "Compute"
-#     },
-#     var.ec2_tags
-#   )
