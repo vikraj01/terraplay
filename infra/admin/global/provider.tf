3fc3b3001a44f8538fa942f1c12fee34390908cc
vikashjman <vikaskumar@jmangroup.com>
2024-10-05 23:48:06 +0530

    feat: updated the dynamodb_secrets

--- infra/admin/global/provider.tf
+++ infra/admin/global/provider.tf
@@ -6,7 +6,7 @@ terraform {
       version = "~> 5.0"
     }
     null = {
-      source  = "hashicorp/null"
+      source = "hashicorp/null"
       version = "3.2.3"
     }
     archive = {
@@ -15,7 +15,12 @@ terraform {
     }
   }
 
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "terraplay-keeper-backend-da28ee014ea0433f"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-state-lock"
+    key            = "terraform.tfstate"
+  }
 }
 
 provider "aws" {
