a43fd7bce09e4f5a91b21d5063b8925fc4f28469
vikashjman <vikaskumar@jmangroup.com>
2024-10-05 23:20:12 +0530

    feat: update global infra action

--- infra/admin/keeper/main.tf
+++ infra/admin/keeper/main.tf
@@ -1,17 +1,26 @@
+# -------------------------
+# Random ID for S3 Bucket Prefix
+# -------------------------
 resource "random_id" "bucket_prefix" {
   byte_length = 8
 }
 
+# -------------------------
+# Backend Storage Module for S3
+# -------------------------
 module "backend_storage" {
   source      = "../../modules/storage"
   bucket_name = "${var.project_name}-backend-${random_id.bucket_prefix.hex}"
 
   tags = merge(local.storage_tags, {
-    Name = "${var.project_name}-backend-${random_id.bucket_prefix.hex}",
+    Name      = "${var.project_name}-backend-${random_id.bucket_prefix.hex}",
     ManagedBy = var.managed_by
   })
 }
 
+# -------------------------
+# DynamoDB Table for Terraform State Lock
+# -------------------------
 module "terraform_state_lock" {
   source         = "../../modules/database/dyanmodb"
   table_name     = var.table_name
@@ -22,17 +31,23 @@ module "terraform_state_lock" {
   range_key_type = var.range_key_type
 
   tags = merge(local.dynamodb_tags, {
-    Name = var.table_name,
+    Name      = var.table_name,
     ManagedBy = var.managed_by
   })
 }
 
+# -------------------------
+# IAM OpenID Connect Provider for GitHub Actions
+# -------------------------
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
+# -------------------------
+# IAM Role for GitHub Actions
+# -------------------------
 resource "aws_iam_role" "github_actions_role" {
   name = "GithubActionsRole"
 
@@ -55,6 +70,9 @@ resource "aws_iam_role" "github_actions_role" {
   })
 }
 
+# -------------------------
+# IAM Role Policy for GitHub Actions Role
+# -------------------------
 resource "aws_iam_role_policy" "github_actions_policy" {
   role = aws_iam_role.github_actions_role.id
 
