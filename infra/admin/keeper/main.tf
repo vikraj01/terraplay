3345e42a1fef736dd14ff27f0dd1d4dc0284092f
vikashjman <vikaskumar@jmangroup.com>
2024-10-05 20:49:32 +0530

    feat: added oidc and user

--- infra/admin/keeper/main.tf
+++ infra/admin/keeper/main.tf
@@ -1,12 +1,14 @@
 resource "random_id" "bucket_prefix" {
   byte_length = 8
 }
+
 module "backend_storage" {
   source      = "../../modules/storage"
   bucket_name = "${var.project_name}-backend-${random_id.bucket_prefix.hex}"
 
   tags = merge(local.storage_tags, {
-    Name = "${var.project_name}-backend-${random_id.bucket_prefix.hex}"
+    Name = "${var.project_name}-backend-${random_id.bucket_prefix.hex}",
+    ManagedBy = var.managed_by
   })
 }
 
@@ -20,6 +22,81 @@ module "terraform_state_lock" {
   range_key_type = var.range_key_type
 
   tags = merge(local.dynamodb_tags, {
-    Name = var.table_name
+    Name = var.table_name,
+    ManagedBy = var.managed_by
+  })
+}
+
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+resource "aws_iam_role" "github_actions_role" {
+  name = "github-actions-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" : "repo:${var.github_repo}:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "github_actions_policy" {
+  role = aws_iam_role.github_actions_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "s3:ListBucket",
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ],
+        Resource = [
+          "arn:aws:s3:::${module.backend_storage.bucket_name}",
+          "arn:aws:s3:::${module.backend_storage.bucket_name}/*"
+        ]
+      },
+      {
+        Effect   = "Allow",
+        Action   = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:DeleteItem",
+          "dynamodb:UpdateItem"
+        ],
+        Resource = "arn:aws:dynamodb:${var.region}:${var.account_id}:table/${module.terraform_state_lock.table_name}"
+      },
+      {
+        Effect   = "Allow",
+        Action   = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ],
+        Resource = "arn:aws:ecr:${var.region}:${var.account_id}:repository/*"
+      }
+    ]
   })
 }
