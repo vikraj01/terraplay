6ef745839345a6a3eca587218ba3076b7c4f18de
vikashjman <vikaskumar@jmangroup.com>
2024-10-05 23:31:42 +0530

    feat: update the session

--- pkg/models/session.go
+++ pkg/models/session.go
@@ -13,5 +13,4 @@ type Session struct {
 	CreatedAt  time.Time `json:"created_at"`
 	UpdatedAt  time.Time `json:"updated_at"`
 	WorkSpace  string    `json:"workspace"`
-	InstanceId string    `json:"instance_id"`
 }
