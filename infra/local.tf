9e51f1cbd65a79d7a1622c93928f2abeac021dc2
vikashjman <vikaskumar@jmangroup.com>
2024-10-06 21:24:16 +0530

    feat: update readme.md

--- infra/local.tf
+++ infra/local.tf
@@ -9,5 +9,9 @@ locals {
   workspace_name  = terraform.workspace
   split_workspace = split("@", local.workspace_name)
   workspace_game  = length(local.split_workspace) > 1 ? local.split_workspace[1] : ""
+<<<<<<< Updated upstream
   valid_game      = contains(["minetest"], lower(local.workspace_game)) ? local.workspace_game : ""
+=======
+  valid_game      = contains(["minetest,terraria,valheim"], lower(local.workspace_game)) ? local.workspace_game : ""
+>>>>>>> Stashed changes
 }
