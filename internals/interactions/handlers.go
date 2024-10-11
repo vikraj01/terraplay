89b6639beb41b43cfcc0d54c5f1056996a618375
vikashjman <vikaskumar@jmangroup.com>
2024-10-11 19:28:35 +0530

    feat: handle modal submission actions

--- internals/interactions/handlers.go
+++ internals/interactions/handlers.go
@@ -3,10 +3,17 @@ package interactions
 import "github.com/bwmarrin/discordgo"
 
 func InteractionHandlers(s *discordgo.Session, i *discordgo.InteractionCreate) {
-        if i.Type == discordgo.InteractionApplicationCommand {
-                switch i.ApplicationCommandData().Name {
-                case "create":
-                        HandleCreateInteraction(s, i)
-                }
+    if i.Type == discordgo.InteractionApplicationCommand {
+        switch i.ApplicationCommandData().Name {
+        case "create":
+            HandleCreateInteraction(s, i)
         }
+    } else if i.Type == discordgo.InteractionModalSubmit {
+        switch i.ModalSubmitData().CustomID {
+        case "create_game_modal":
+            HandleModalSubmit(s, i)
+        }
+    }
 }
+
+
