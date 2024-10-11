a7d980ff74c37d79f1b634c58d4664ff8a3960c0
vikashjman <vikaskumar@jmangroup.com>
2024-10-11 19:20:31 +0530

    feat: update the create app

--- internals/interactions/create.go
+++ internals/interactions/create.go
@@ -1,47 +1,77 @@
 package interactions
 
 import (
-        "fmt"
-
-        "github.com/bwmarrin/discordgo"
+    "fmt"
+    "github.com/bwmarrin/discordgo"
 )
 
 func CreateInteraction() *discordgo.ApplicationCommand {
     return &discordgo.ApplicationCommand{
         Name:        "create",
         Description: "Create a new game server",
-        Options: []*discordgo.ApplicationCommandOption{
-            {
-                Name:        "name",
-                Description: "The name of the game server",
-                Type:        discordgo.ApplicationCommandOptionString,
-                Required:    true,
-            },
-            {
-                Name:        "max_players",
-                Description: "Maximum number of players",
-                Type:        discordgo.ApplicationCommandOptionInteger,
-                Required:    false,
-            },
-        },
     }
 }
 
 func HandleCreateInteraction(s *discordgo.Session, i *discordgo.InteractionCreate) {
-    options := i.ApplicationCommandData().Options
-    serverName := options[0].StringValue()
-    var maxPlayers int64 = 10
-
-    if len(options) > 1 {
-        maxPlayers = options[1].IntValue()
-    }
-
-    response := fmt.Sprintf("Creating server '%s' with max players: %d", serverName, maxPlayers)
     s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-        Type: discordgo.InteractionResponseChannelMessageWithSource,
+        Type: discordgo.InteractionResponseModal,
         Data: &discordgo.InteractionResponseData{
-            Content: response,
+            CustomID: "create_game_modal",
+            Title:    "Create Game Server",
+            Components: []discordgo.MessageComponent{
+                discordgo.ActionsRow{
+                    Components: []discordgo.MessageComponent{
+                        discordgo.TextInput{
+                            CustomID:    "server_name",
+                            Label:       "Server Name",
+                            Style:       discordgo.TextInputShort,
+                            Placeholder: "Enter the name of the server",
+                            Required:    true,
+                        },
+                    },
+                },
+                discordgo.ActionsRow{
+                    Components: []discordgo.MessageComponent{
+                        discordgo.TextInput{
+                            CustomID:    "max_players",
+                            Label:       "Maximum Players",
+                            Style:       discordgo.TextInputShort,
+                            Placeholder: "Enter max players (default is 10)",
+                            Required:    false,
+                        },
+                    },
+                },
+            },
         },
     })
 }
 
+func HandleModalSubmit(s *discordgo.Session, i *discordgo.InteractionCreate) {
+    if i.Type == discordgo.InteractionModalSubmit && i.ModalSubmitData().CustomID == "create_game_modal" {
+        var serverName string
+        var maxPlayers int64 = 10
+
+        for _, component := range i.ModalSubmitData().Components {
+            row := component.(*discordgo.ActionsRow)
+            for _, input := range row.Components {
+                textInput := input.(*discordgo.TextInput)
+                switch textInput.CustomID {
+                case "server_name":
+                    serverName = textInput.Value
+                case "max_players":
+                    if textInput.Value != "" {
+                        fmt.Sscanf(textInput.Value, "%d", &maxPlayers)
+                    }
+                }
+            }
+        }
+
+        response := fmt.Sprintf("Creating server '%s' with max players: %d", serverName, maxPlayers)
+        s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+            Type: discordgo.InteractionResponseChannelMessageWithSource,
+            Data: &discordgo.InteractionResponseData{
+                Content: response,
+            },
+        })
+    }
+}
